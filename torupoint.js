f2e1b3de6472e3b6179d6ddf4718051e76580c35
Nuieiei <158017511+Nuieiei@users.noreply.github.com>
2024-06-01 20:15:36 +0700

    Add files via upload

---
+++ torupoint.js
@@ -0,0 +1,156 @@
+var tourpoint =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เสาชิง",
+          "province": "จังหวัด",
+          "type": "counter"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50129239060419,
+            13.751886166969157
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอพระนาก",
+          "province": "จังหวัด",
+          "type": "counter"
+        },
+        "geometry": {
+          "coordinates": [
+            100.49258868336358,
+            13.751633344847491
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สยามพารากอน",
+          "province": "จังหวัด",
+          "type": "special"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5349571160832,
+            13.746692010157403
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนวชิรเบญจทัศ",
+          "province": "จังหวัด",
+          "type": "ืnatural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.55352954232956,
+            13.815695945838854
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดสระเกศ ราชวรมหาวิหาร",
+          "province": "จังหวัด",
+          "type": "counter"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50708195767476,
+            13.753782368881033
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ป่าในกรุง",
+          "province": "จังหวัด",
+          "type": "ืnatural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.70282972883513,
+            13.691867221371595
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "แฟชั่นไอส์แลนด์",
+          "province": "จังหวัด",
+          "type": "special"
+        },
+        "geometry": {
+          "coordinates": [
+            100.67242000000113,
+            13.838548362571558
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนลอยฟ้าเจ้าพระยา",
+          "province": "จังหวัด",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.49839528233116,
+            13.738853419319085
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนเบญจกิติ",
+          "province": "จังหวัด",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.55852054232577,
+            13.730605792832705
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนหลวง",
+          "province": "จังหวัด",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52408181349392,
+            13.74205347837686
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
